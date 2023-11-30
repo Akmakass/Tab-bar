513dae632b020786a41cf731a29d92949cf751b5
Assel Akmakanova <akmakass@gmail.com>
2023-11-30 17:29:31 +0600

    first commit

---
+++ ToDoList2/TaskItem.swift
@@ -0,0 +1,13 @@
+//
+//  TaskItem.swift
+//  ToDoList2
+//
+//  Created by Assel Akmakanova on 30.11.2023.
+//
+
+import Foundation
+
+struct TaskItem: Codable {
+    var name = ""
+    var isComplete = false
+}
